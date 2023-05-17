dbb5dcb51a2304117a2085d97dce706d06594354
wuchunxin <wuyutong0908@gmail.com>
2023-05-17 16:04:59 +0800

    auto generate methods

--- gen.go
+++ gen.go
@@ -1,19 +1,25 @@
 package main
 
 import (
+	"github.com/springstar/confgen/config"
+	_ "reflect"
 	"fmt"
-	"encoding/json"
+	jsoniter "github.com/json-iterator/go"
 	"io/ioutil"
 	"path/filepath"
 	"strings"
 	"os"
 	"log"
 	"github.com/m-zajac/json2go"
+	"github.com/gobuffalo/plush"
 
 )
 
+var json = jsoniter.ConfigCompatibleWithStandardLibrary
+
 type JsonConfig struct {
 	name string
+	// struct slice, every map is a struct, like ConfAI, ConItem etc
 	data []map[string]interface{}
 }
 
@@ -36,28 +42,61 @@ type JsonConfigManager struct {
 }
 
 func NewJsonConfigManager() *JsonConfigManager {
-	return &JsonConfigManager{}
+	return &JsonConfigManager{
+		confs: make(map[string]*JsonConfig),
+	}
 }
 
-func (m *JsonConfigManager) AddConf(name string, conf *JsonConfig) {
-	if _, ok := m.confs[name]; !ok {
+func (m *JsonConfigManager) loadConf(path string) {
+	files, err := ioutil.ReadDir(path)
+	if (err != nil) {
+		log.Fatal(err)
+	}
+
+	for _, f := range files {
+		ext := filepath.Ext(f.Name())
+		if strings.Compare(ext, ".json") != 0 {
+			continue
+		}
+
+		fname := filepath.Join(path, f.Name())
+		content, err := ioutil.ReadFile(fname)
+		if (err != nil) {
+			log.Fatal(err)
+		}
+
+		conf := NewJsonConfig(f.Name())
+		conf.Parse(content)
+		k := strings.TrimSuffix(f.Name(), filepath.Ext(f.Name()))
+		m.addConf(k, conf)
+	}
+}
+
+func (m *JsonConfigManager) addConf(name string, conf *JsonConfig) {
+	if _, ok := m.confs[name]; ok {
 		return
 	}
 
 	m.confs[name] = conf
 }
 
+func (m *JsonConfigManager) findConf(name string, sn int) interface{} {
+	if conf, ok := m.confs[name]; ok {
+		return conf
+	}
+
+	return nil
+}
+
 type ConfigGen struct {
 
 }
 
-func genStruct() string {
+func genStructs() (names []string, defines []string) {
 	files, err := ioutil.ReadDir("config")
 	if (err != nil) {
 		log.Fatal(err)
 	}
-
-	var s []string
 
 	for _, f := range files {
 		ext := filepath.Ext(f.Name())
@@ -66,6 +105,7 @@ func genStruct() string {
 		}
 
 		sname, _ := strings.CutSuffix(f.Name(), ".json")
+		names = append(names, sname)
 
 		file := filepath.Join("config", f.Name())
 		content, _ := ioutil.ReadFile(file)
@@ -73,16 +113,40 @@ func genStruct() string {
 		parser.FeedBytes(content)
 		res := parser.String()
 		out := strings.Replace(res, "[]", " ", 1)
-		s = append(s, out)
+		defines = append(defines, out)
 	}
 
-	str := strings.Join(s, "\n\n")
-	return str
+	return names, defines
 	
 }
 
-func write(s string) {
-	file, err := os.Create("config/gen/structs.go")
+func writeMethods(names []string) {
+	content, err := ioutil.ReadFile("template/struct.tpl")
+	if (err != nil) {
+		log.Fatal(err)
+	}
+
+	template := string(content[:])
+	ctx := plush.NewContext()
+	ctx.Set("names", names)
+
+	s, err := plush.Render(template, ctx)
+	if (err != nil) {
+		log.Fatal(err)
+	}
+
+	file, err := os.Create("config/loader.go")
+    if err != nil {
+        return
+    }
+    defer file.Close()
+
+    file.WriteString(s)
+}
+
+func writeStructs(str []string) {
+	s := strings.Join(str, "\n\n")
+	file, err := os.Create("config/structs.go")
 	if err != nil {
 		fmt.Println(err)
 		return
